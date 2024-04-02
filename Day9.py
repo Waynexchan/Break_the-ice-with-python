dae35b4fea04ee7884a780c003042b0f2243c650
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-02 18:22:28 +0000

    Day9 update

--- Day9.py
+++ Day9.py
@@ -1,6 +1,27 @@
+'''
+Question:
+Define a function which can compute the sum of two numbers.
+
+Hints:
+Define a function with two numbers as arguments. You can compute the sum in the function and return the value.
+
 def add():
   num1, num2 = int(input()),int(input())
   total = num1+num2
   print(total)
 
 add()
+
+'''
+'''
+Define a function that can convert a integer into a string and print it in console.
+
+Hints:
+Use str() to convert a number to string.
+
+def to_s():
+  s = str(int(input()))
+  print(s)
+
+to_s()
+'''
