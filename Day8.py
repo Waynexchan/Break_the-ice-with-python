abda7940416b13f94da1e7479231753da8dbe4a1
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-02 14:49:04 +0000

    Day8

--- Day8.py
+++ Day8.py
@@ -33,3 +33,38 @@ def count_frequency():
 count_frequency()
 
 '''
+'''
+Question:
+Write a method which can calculate square value of number
+
+Hints:
+Using the ** operator which can be written as n**p where means n^p
+
+def sqt():
+  n = int(input())
+  value =n ** 2
+  print(value)
+
+sqt()
+'''
+'''
+Question:
+Python has many built-in functions, and if you do not know how to use it, you can read document online or find some books. But Python has a built-in document function for every built-in functions.
+
+Please write a program to print some Python built-in functions documents, such as abs(), int(), raw_input()
+
+And add document for your own function
+
+Hints:
+The built-in document method is __doc__
+'''
+
+def get_value():
+  '''
+  convert input in to a absolute integer
+  '''
+  no = abs(int(input()))
+  print(no)
+  print(get_value.__doc__)
+
+get_value()
