b17ff4ad1ee751221a890d3f2e29093bcb61313a
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-07 09:17:34 +0000

    Day14.py

--- Day14.py
+++ Day14.py
@@ -1,3 +1,4 @@
+'''
 def test():
   try:
     a, b = int(input()),int(input())
@@ -10,3 +11,22 @@ def test():
 
 
 test()
+'''
+
+class CustomException(Exception):
+  '''
+    Attributes:
+      message:
+  '''
+  def __init__(self,message):
+    self.message = message
+
+num = int(input())
+
+try:
+  if num < 10:
+    raise CustomException(" num is less than 10")
+  elif num >20:
+    raise CustomException(" num is greater than 10")
+except CustomException as err:
+  print('The error raises: '+ err.message)
