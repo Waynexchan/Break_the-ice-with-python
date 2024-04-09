6ec5f0bd910dafae6676967c50857a8ae4ea167a
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-09 14:15:47 +0000

    Day16 QS 63

--- Day16.py
+++ Day16.py
@@ -34,4 +34,14 @@ n = int(input('please enter n: '))
 print_flist(n)
 '''
 
-  
+def even(n):
+  i = 0
+  while i < n+1:
+    if i %2 == 0:
+      yield i
+    i+=1
+
+n = int(input())
+for num in even(n):
+  print(num)
+    
