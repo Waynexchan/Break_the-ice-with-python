5e187915d06a39239ee7b814ac30071e60fd0ccb
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-10 11:20:50 +0000

    Day17 qs 66

--- Day17.py
+++ Day17.py
@@ -1,3 +1,4 @@
+'''
 def even():
   s= input()
   s_li = s.split(',')
@@ -6,3 +7,13 @@ def even():
     assert int(i)%2 ==0, 'It contains odds number'
 
 even()
+'''
+
+
+def expression():
+  expression = input()
+  result = eval(expression)
+
+  print(result)
+
+expression()
