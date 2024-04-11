738deb03cb6c355666bca0117291046ddf6e5282
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-11 08:05:23 +0000

    Day18 qs71

--- Day18.py
+++ Day18.py
@@ -1,5 +1,6 @@
 import random
 
+'''
 def ran():
   s= int(input())
   e= int(input())
@@ -7,3 +8,13 @@ def ran():
   print(li)
 
 ran()
+'''
+
+def ran():
+  s= int(input())
+  e= int(input())
+  li =random.choice([x for x in range(s,e+1,2) if x %5 ==0 and x %7==0])
+  print(li)
+
+ran()
+
