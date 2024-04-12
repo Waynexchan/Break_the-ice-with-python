60e89744784e5d229d810c3f9213664f01890326
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-12 11:06:05 +0000

    Day19 qs76

--- Day19.py
+++ Day19.py
@@ -1,5 +1,6 @@
 import random
-
+import zlib
+'''
 def ran():
   s= int(input())
   e= int(input())
@@ -7,3 +8,9 @@ def ran():
   print(random.randrange(s, e+1))
 
 ran()
+'''
+s='hello world!hello world!hello world!hello world!'
+y = bytes(s, 'utf-8')
+t= zlib.compress(y)
+print(t)
+print(zlib.decompress(t))
