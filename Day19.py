172fab81d0772fd530d6c71402e8daa6c88c11c5
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-12 11:11:04 +0000

    Day19 qs77

--- Day19.py
+++ Day19.py
@@ -9,8 +9,21 @@ def ran():
 
 ran()
 '''
+'''
 s='hello world!hello world!hello world!hello world!'
 y = bytes(s, 'utf-8')
 t= zlib.compress(y)
 print(t)
 print(zlib.decompress(t))
+'''
+
+import datetime
+
+before = datetime.datetime.now()
+
+for i in range(100):
+  x=1+1
+
+after = datetime.datetime.now()
+execution_time = after - before
+print(execution_time)
