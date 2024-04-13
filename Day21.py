69164d668f5127d0422e315cf620074c4d2accd7
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-13 22:27:26 +0000

    Day21

--- Day21.py
+++ Day21.py
@@ -10,10 +10,19 @@ li=  [12,24,35,24,88,120,155]
 new_li =[x for x in li if x != 24]
 print(new_li)
 '''
-
+'''
 li = [1,3,6,78,35,55] 
 lib = [12,24,35,24,88,120,155]
 s1= set(li)
 s2= set(lib)
 intersection = set.intersection(s1,s2)
 print(intersection)
+'''
+
+li= [12,24,35,24,88,120,155,88,120,155]
+s1= set(li)
+new_li =[]
+for x in s1:
+  new_li.append(x)
+
+print(new_li)
