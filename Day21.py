e0c3f29fb2e3dec0d1d85446a1ea0960663c6f03
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-13 22:20:46 +0000

    Day21

--- Day21.py
+++ Day21.py
@@ -5,7 +5,15 @@ index= [0,4,5]
 new_li= list(x for (i,x) in enumerate(li) if i  not in index)
 print(new_li)
 '''
-
+'''
 li=  [12,24,35,24,88,120,155]
 new_li =[x for x in li if x != 24]
 print(new_li)
+'''
+
+li = [1,3,6,78,35,55] 
+lib = [12,24,35,24,88,120,155]
+s1= set(li)
+s2= set(lib)
+intersection = set.intersection(s1,s2)
+print(intersection)
