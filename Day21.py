d3b3285358ef1a67d4ef38d5b486a5b666311a88
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-13 22:12:19 +0000

    Day21 qs 86

--- Day21.py
+++ Day21.py
@@ -1,5 +1,11 @@
+'''
 s = input()
 li = s.split()
 index= [0,4,5]
 new_li= list(x for (i,x) in enumerate(li) if i  not in index)
+print(new_li)
+'''
+
+li=  [12,24,35,24,88,120,155]
+new_li =[x for x in li if x != 24]
 print(new_li)
