f5dab0230120ad02f7365fd4520fe65e4a53a924
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-13 14:56:45 +0000

    Day20 qs 81

--- Day20.py
+++ Day20.py
@@ -1,3 +1,9 @@
 li= [5,6,77,45,22,12,24]
 new_li =[x for x in li if x %2 !=0 ]
 print(new_li)
+
+li2 = [12,24,35,70,88,120,155]
+
+newli2= list(filter(lambda x : x%5 !=0 and x%7 !=0, li2))
+
+print(newli2)
