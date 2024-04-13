9bda7da2d77fbd2fe2330d5e75ccf18e968d9804
Wayne Chan <162703158+Waynexchan@users.noreply.github.com>
2024-04-13 15:04:16 +0000

    Day20 qs 82

--- Day20.py
+++ Day20.py
@@ -1,3 +1,4 @@
+'''
 li= [5,6,77,45,22,12,24]
 new_li =[x for x in li if x %2 !=0 ]
 print(new_li)
@@ -7,3 +8,9 @@ li2 = [12,24,35,70,88,120,155]
 newli2= list(filter(lambda x : x%5 !=0 and x%7 !=0, li2))
 
 print(newli2)
+'''
+
+li= [12,24,35,70,88,120,155]
+index = [0, 2, 4, 6]
+new_li = [i for (j, i) in enumerate(li) if j not in index ]
+print(new_li)
